415cf57a881b103162923d828126a75a5591674b
MarjaCarty <marja.d.carty@gmail.com>
2020-11-13 10:33:11 -0600

    login working, axiosWithAuth and PrivateRoute built

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const initialCredentials = {
   username: "",
   password: "",
 };
 
-const Login = () => {
+const Login = (props) => {
   const [credentials, setCredentials] = useState(initialCredentials);
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
@@ -15,23 +16,42 @@ const Login = () => {
     setCredentials({ ...credentials, [name]: value });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", credentials)
+      .then((res) => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/bubble-page");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1>Welcome to the Bubble App!</h1>
-      <input
-        type="text"
-        name="username"
-        value={credentials.username}
-        placeholder="Enter Username"
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        value={credentials.password}
-        placeholder="Enter Password"
-        onChange={handleChange}
-      />
+      <label>
+        Username:
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          placeholder="Enter Username"
+          onChange={handleChange}
+        />
+      </label>
+
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          placeholder="Enter Password"
+          onChange={handleChange}
+        />
+      </label>
+
       <button type="submit">Login</button>
     </form>
   );
